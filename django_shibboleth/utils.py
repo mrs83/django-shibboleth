882b9a98f74e74f8a45a4561965c7267e3bbf27d
Eric Stein <ericst@wharton.upenn.edu>
2014-01-31 11:09:32 -0500

    Added basic functionality for including configurations from other schools

--- django_shibboleth/utils.py
+++ django_shibboleth/utils.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with django_shibboleth  If not, see <http://www.gnu.org/licenses/>.
+import json
 
 from django.conf import settings
 from django.shortcuts import render_to_response
@@ -23,7 +24,13 @@ from django.http import HttpResponseRedirect
 def parse_attributes(META):
     shib_attrs = {}
     error = False
-    for header, attr in settings.SHIB_ATTRIBUTE_MAP.items():
+    if settings.SHIB_MULTISCHOOL:
+        school = META.get('school_identifier_attribute')  # This probably isn't correct. Fix it later.
+        mapping = json.loads(settings.PATH_TO_SHIB_SETTINGS)[school]
+    else:
+        mapping = settings.SHIB_ATTRIBUTE_MAP
+
+    for header, attr in mappings.items():
         required, name = attr
         values = META.get(header, None)
         value = None
